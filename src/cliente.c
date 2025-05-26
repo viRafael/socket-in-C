0c0987f206a806f2d34862a32bdb4d00ae870545
Rafael Vieira <mouravieira44@gmail.com>
2025-05-26 18:43:26 -0300

    att

---
+++ src/cliente.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    // Criar um socket
+    // int server = socket(int domain, int type, int protocol);
+
+    // Conectar ao servidor
+
+    // Enviar a mensagem "READY" para o servidor
+
+    // Recebeu a mensagem "READY ACK" do servidor
+}
